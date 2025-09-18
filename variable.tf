3a28de5563ee00edf39d64fccf9657195c22716f
ankitecb <ankits.ecb@gmail.com>
2025-09-18 23:21:04 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "bucket_prefix" {
+  description = "Prefix for S3 bucket names"
+  type        = string
+  default     = "my-terraform-bucket"
+}
+
+variable "environment" {
+  description = "Default environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "environments" {
+  description = "List of environments to create buckets for"
+  type        = list(string)
+  default     = ["dev", "qa", "prod"]
+}
