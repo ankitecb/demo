42bc5d80b01141d9469d73cdfa7a0cfb5402b18a
ankitecb <ankits.ecb@gmail.com>
2025-09-19 13:49:16 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -38,25 +38,3 @@ terraform {
 
 
 data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "example" {
-
-bucket = "my-demoo-bucket-${data.aws_caller_identity.current.account_id}"
-}
-
-
-output "a3_bucket_arn" {
-  value = aws_s3_bucket.example.arn
-}
-
-
-
-
-resource "aws_instance" "web" {
-  ami           = "ami-0fa8fe6f147dc938b"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
