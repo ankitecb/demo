b926bcf51e4e68c83dacaafd45afa0d92483b724
ankitecb <ankits.ecb@gmail.com>
2025-09-19 13:48:00 +0530

    Update and rename demo.tf to Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,7 +1,19 @@
+#################################################
+# Terraform & Provider Setup
+#################################################
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-
-  region = "eu-west-1"  # Replace with your desired AWS region
-
+  region = var.region
 }
 
 terraform {
