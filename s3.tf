0841a6ff49e83d2e56f2c49a10ed7358e302168c
ankitecb <ankits.ecb@gmail.com>
2025-09-19 13:47:26 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,20 +1,3 @@
-#################################################
-# Terraform & Provider Setup
-#################################################
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
 
 #################################################
 # Data Source (read-only information from AWS)
